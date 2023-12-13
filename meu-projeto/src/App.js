cf22e04def09f9fcd4604e8af8256cf2bfdea3c6
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2023-12-13 12:04:50 -0300

    criei e importei um componente

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Opa</h1>
+
+      <HelloWorld />
     </div>
   );
 }
