9643232bfdc484e9365c547526baca57c8d7e912
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2023-12-14 11:29:46 -0300

    validando os componente usando PorpTypes

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,10 +1,24 @@
-function Item(props) {
+import PropTypes from 'prop-types'
+
+function Item({ marca, ano_lancamento}) {
     return (
         <>
-            <li>{props.marca}</li>
+            <li>
+                {marca} - {ano_lancamento}
+            </li>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano_lancamento: PropTypes.number
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lancamento: 0,
 }
 
 export default Item
